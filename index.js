ace58e3fca0eaf022e9f4a31becff35f16fe1fa0
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-03-09 21:11:55 -0500

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+
+
+server.listen(2000, () => {
+    console.log('\n*** Server Running on http://localhost:2000 ***\n');
+
+
+})
